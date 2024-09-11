f1b0805018711809e871b399acc4d721b87929fb
Daniel Balolong <daniel_balolong@sbcglobal.net>
2024-09-11 15:16:08 -0700

    Fix missing () at require dotenv

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,6 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
-require('dotenv').config
+//const { clientId, guildId, token } = require('./config.json');
+require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
 
